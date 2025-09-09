a0e5cc5174de2f63206c6976dde3da9f45d9213d
toluhunter <toluhunterdev@gmail.com>
2025-09-09 18:28:25 +0000

    change position of video position

--- src/components/pages/about/about.tsx
+++ src/components/pages/about/about.tsx
@@ -25,24 +25,27 @@ export const AboutSection = () => {
   return (
     <section className="min-h-screen pt-25 w-full container mx-auto flex flex-col items-center px-8 pb-10">
       <h1 className="flex w-full justify-center font-bold text-6xl">About Me</h1>
-      
-      {/* Experience Section - Always Visible */}
-      <div className="w-full pt-10">
-        <p className="text-md md:text-xl w-full">
-          Heya, yup that’s me 👀. I’m Tolulope Fakoya a cloud engineer with 3+ years of experience building and designing cloud setups (AWS is my main playground, but I’ve explored others too). I also work as a freelance full-stack developer and spend part of my free time geeking out as a pentester. This site itself runs on some of the same tech I enjoy working with. At the end of the day, I just love taking an idea and making it real because like the quote says, “Engineers turn ideas into reality.” That’s exactly what I aim to do.
-        </p>
-        <div className="flex flex-col md:flex-row gap-5 items-center mt-10">
-          <FaGithub /> <Link href="https://github.com/toluhunter" target="_blank"><span className="text-lg">Github</span></Link>
-          <FaLinkedin /> <Link href="https://www.linkedin.com/in/tolulope-fakoya/" target="_blank"> <span className="text-lg">linkedin</span></Link>
+
+      <div className="flex flex-col lg:flex-row gap-5">
+
+        {/* Experience Section - Always Visible */}
+        <div className="w-full pt-10">
+          <p className="text-md md:text-xl w-full">
+            Heya, yup that’s me 👀. I’m Tolulope Fakoya a cloud engineer with 3+ years of experience building and designing cloud setups (AWS is my main playground, but I’ve explored others too). I also work as a freelance full-stack developer and spend part of my free time geeking out as a pentester. This site itself runs on some of the same tech I enjoy working with. At the end of the day, I just love taking an idea and making it real because like the quote says, “Engineers turn ideas into reality.” That’s exactly what I aim to do.
+          </p>
+          <div className="flex flex-col md:flex-row gap-5 items-center mt-10">
+            <FaGithub /> <Link href="https://github.com/toluhunter" target="_blank"><span className="text-lg">Github</span></Link>
+            <FaLinkedin /> <Link href="https://www.linkedin.com/in/tolulope-fakoya/" target="_blank"> <span className="text-lg">linkedin</span></Link>
+          </div>
+        </div>
+
+        {/* Video/3D Model Placeholder */}
+        <div className="flex w-full h-[300px] bg-gray-200 dark:bg-gray-800 mt-10 rounded-lg">
+          {/* Placeholder for future content like a 3D model or image */}
         </div>
       </div>
 
-      {/* Video/3D Model Placeholder */}
-      <div className="flex w-full h-[300px] bg-gray-200 dark:bg-gray-800 mt-10 rounded-lg">
-         {/* Placeholder for future content like a 3D model or image */}
-      </div>
-
-      <div id="blog-detail" className="flex flex-col lg:flex-row items-start justify-center w-full gap-8 pt-10">
+      <div id="blog-detail" className="flex flex-col lg:flex-row mx-auto items-center justify-center w-full gap-8 pt-10">
 
         {/* Sidebar / Horizontal Bar */}
         <div className="w-full lg:w-auto">
@@ -51,11 +54,10 @@ export const AboutSection = () => {
               <button
                 key={section}
                 onClick={() => setActiveSection(section)}
-                className={`p-2 px-4 text-center rounded-lg border border-callout text-callout transition-colors duration-300 ${
-                  activeSection === section
+                className={`p-2 px-4 text-center rounded-lg border border-callout text-callout transition-colors duration-300 ${activeSection === section
                     ? 'bg-callout text-white'
                     : 'bg-transparent hover:bg-callout/20'
-                }`}
+                  }`}
               >
                 {section}
               </button>
